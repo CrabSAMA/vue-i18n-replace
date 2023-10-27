973d38176a09a82de182bb43b3d3307547e1189b
crab.huang <crab.huang@huolala.cn>
2023-10-27 20:08:53 +0800

    feat: vue2 setup import 实现

--- src/transformer/javascript.ts
+++ src/transformer/javascript.ts
@@ -20,10 +20,18 @@ export async function javascriptTransformer(rawContent: string) {
       resetLastIndex()
       if (chinesePattern.test(path.node.value) && !i18nStartKeyPattern.test(parentCode)) {
         // vue3 t
-        // TODO vue3 还要加上 import 语句
-        // vue2 this.$t
+        // TODO vue3 还要加上 import 语句，vue2 setup script 也要加上 getCurrentInstance
+        // vue2 normal script: this.$t / setup script: instance.proxy.$t 
         path.replaceWith(t.callExpression(t.identifier("$t"), [t.stringLiteral(path.node.value)]))
         path.skip()
+      }
+    },
+    ImportDeclaration(path) {
+      if (path.node.source.value === 'vue') {
+        const importVueList = path.node.specifiers.map((item) => item.local.name)
+        if (!importVueList.includes('getCurrentInstance')) {
+          path.node.specifiers.push(t.importSpecifier(t.identifier('getCurrentInstance'), t.identifier('getCurrentInstance')))
+        }
       }
     }
   })
@@ -35,6 +43,7 @@ export async function javascriptTransformer(rawContent: string) {
     }
   })
   console.log(output.code);
+  return output.code
 }
 
 const content = `
